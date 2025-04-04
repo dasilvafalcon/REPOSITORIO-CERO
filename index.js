5dffa461c53b8b26ffd20535adf5c4f36b86a323
dasilvafalcon <Berley783@gmail.com>
2025-04-03 22:12:29 -0300

    estabamos perdidos con la tarea, pero hubo una luz

---
+++ index.js
@@ -0,0 +1,12 @@
+let usuario = {
+    nombre : "berley",
+    apellido: "dasilcon",
+    edad: 45 ,
+    ci: 27814567,
+    calificacion:  10
+} //Objeto 
+console.log (usuario.)
+/*
+let nombre// variable 
+let apellido "dasilcon"
+*/dir
